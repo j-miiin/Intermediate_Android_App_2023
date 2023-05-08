691bae6aec494ab5aeb4e138574363785e63462c
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-05-08 19:11:18 +0900

    Part02 - Chapter01 Fragment 실습

--- chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/MainActivity.kt
+++ chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.chapter1_webtoon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.example.chapter1_webtoon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        val container = binding.fragmentContainer
+
+        binding.button1.setOnClickListener {
+            val transaction = supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, WebViewFragment())
+                commit()
+            }
+        }
+
+        binding.button2.setOnClickListener {
+
+        }
+
+        setContentView(binding.root)
+
+
     }
 }
