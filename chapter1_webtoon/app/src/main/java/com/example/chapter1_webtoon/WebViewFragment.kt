ba206419791d262e5bea9278ce2350d81c18761f
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-05-09 18:10:55 +0900

    Part02 - Chpater01 마지막 시점 돌아가기 구현

--- chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/WebViewFragment.kt
+++ chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/WebViewFragment.kt
@@ -1,13 +1,16 @@
 package com.example.chapter1_webtoon
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import com.example.chapter1_webtoon.databinding.FragmentWebviewBinding
 
-class WebViewFragment: Fragment() {
+class WebViewFragment(private val position: Int) : Fragment() {
 
     private lateinit var binding: FragmentWebviewBinding
 
@@ -23,9 +26,23 @@ class WebViewFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.webView.webViewClient = WebtoonWebViewClient(binding.progressBar)
+        binding.webView.webViewClient = WebtoonWebViewClient(binding.progressBar) { url ->
+            activity?.getSharedPreferences("WEB_HISTORY", Context.MODE_PRIVATE)?.edit {
+                putString("tab$position", url)
+            }
+        }
         binding.webView.settings.javaScriptEnabled = true
         binding.webView.loadUrl("https://comic.naver.com/webtoon/detail?titleId=764129&no=91")
+
+        binding.backToLastButton.setOnClickListener {
+            val sharedPreference = activity?.getSharedPreferences("WEB_HISTORY", Context.MODE_PRIVATE)
+            val url = sharedPreference?.getString("tab$position", "")
+            if (url.isNullOrEmpty()) {
+                Toast.makeText(context, "마지막 저장 시점이 없습니다.", Toast.LENGTH_SHORT).show()
+            } else {
+                binding.webView.loadUrl(url)
+            }
+        }
     }
 
     fun canGoBack(): Boolean {
--- chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/WebtoonWebViewClient.kt
+++ chapter1_webtoon/app/src/main/java/com/example/chapter1_webtoon/WebtoonWebViewClient.kt
@@ -2,17 +2,23 @@ package com.example.chapter1_webtoon
 
 import android.graphics.Bitmap
 import android.view.View
-import android.webkit.WebResourceError
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.ProgressBar
 
-class WebtoonWebViewClient(private val progressBar: ProgressBar): WebViewClient() {
+class WebtoonWebViewClient(
+    private val progressBar: ProgressBar,
+    private val saveData: (String) -> Unit,
+): WebViewClient() {
 
-//    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-//        return !(request != null && request.url.toString().contains("comic.naver.com"))
-//    }
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        if (request != null && request.url.toString().contains("comic.naver.com/webtoon/detail")) {
+            saveData(request.url.toString())
+        }
+
+        return super.shouldOverrideUrlLoading(view, request)
+    }
 
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
