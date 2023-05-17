da55a3c52164812022628b51f346fa552f1a2b56
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-05-17 18:25:15 +0900

    Part02 - Chapter03 Socket, OkHttp 통신 실습

--- chapter3_today_notice/app/src/main/java/com/example/chapter3_today_notice/MainActivity.kt
+++ chapter3_today_notice/app/src/main/java/com/example/chapter3_today_notice/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.chapter3_today_notice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import okhttp3.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.HttpURLConnection
+import java.net.ServerSocket
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient()
+
+        val request: Request = Request.Builder()
+            .url("http://10.0.2.2:8080")
+            .build()
+
+        val callback = object: Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Client", e.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    Log.e("Client", "${response.body?.string()}")
+                }
+            }
+        }
+
+        client.newCall(request).enqueue(callback)
+
+//        Thread {
+//            try {
+//                val socket = Socket("10.0.2.2", 8080)
+//                val printer = PrintWriter(socket.getOutputStream())
+//                val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+//
+//                printer.println("GET / HTTP/1.1")
+//                printer.println("Host: 127.0.0.1:8080")
+//                printer.println("User-Agent: android")
+//                printer.println("\r\n")
+//                printer.flush()
+//
+//                var input: String? = "-1"
+//                while (input != null) {
+//                    input = reader.readLine()
+//                    Log.e("Client", input)
+//                }
+//                reader.close()
+//                printer.close()
+//                socket.close()
+//            } catch (e: java.lang.Exception) {
+//                Log.e("Client", e.toString())
+//            }
+//        }.start()
     }
 }
---
+++ chapter3_today_notice/app/src/main/java/com/example/chapter3_today_notice/Test.kt
@@ -0,0 +1,39 @@
+package com.example.chapter3_today_notice
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+fun main() {
+    Thread {
+        val port = 8080
+        val server = ServerSocket(port)
+
+        while (true) {
+            val socket = server.accept()
+
+//        socket.getInputStream() // 클라이언트로부터 들어오는 스트림 == 클라이언트의 socket.outputStream
+//        socket.getOutputStream() // 클라이언트에게 데이터를 주는 스트림 == 클라이언트의 socket.inputStream
+
+            val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
+            val printer = PrintWriter(socket.getOutputStream())
+
+            var input: String? = "-1"
+            while (input != null && input != "") input = reader.readLine()
+
+            println("READ DATA $input")
+
+            printer.println("HTTP/1.1 200 OK")
+            printer.println("Content-Type: text/html\r\n")
+            printer.println("<h1>Hello World</h1>")
+            printer.println("\r\n")
+            printer.flush()
+            printer.close()
+
+            reader.close()
+
+            socket.close()
+        }
+    }.start()
+}
