114c38e6d377b55019694b2d939a79729575d67a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-05-25 02:09:41 +0900

    Part02 - Chapter05 TikXml, Rss 코드 작성 중

---
+++ chapter5_news/app/src/main/java/com/example/chapter5_news/NewsRss.kt
@@ -0,0 +1,13 @@
+package com.example.chapter5_news
+
+import com.tickaroo.tikxml.annotation.Xml
+
+@Xml(name = "rss")
+data class NewsRss(
+
+)
+
+data class RssChannel (
+
+)
+
---
+++ chapter5_news/app/src/main/java/com/example/chapter5_news/NewsService.kt
@@ -0,0 +1,9 @@
+package com.example.chapter5_news
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface NewsService {
+    @GET("rss?hl=ko&gl=KR&ceid=KR:ko")
+    fun mainFeed(): Call<NewsEntity>
+}
