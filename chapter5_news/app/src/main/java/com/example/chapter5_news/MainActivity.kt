1d45fa6e27367237a1fb1e720e58b8982f4be86a
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-03 21:38:10 +0900

    Part02 - Chapter05 RecyclerView 클릭시 WebView 띄우기 구현

--- chapter5_news/app/src/main/java/com/example/chapter5_news/MainActivity.kt
+++ chapter5_news/app/src/main/java/com/example/chapter5_news/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.chapter5_news
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -36,7 +37,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        newsAdapter = NewsAdapter()
+        newsAdapter = NewsAdapter { url ->
+            startActivity(
+                Intent(this, WebViewActivity::class.java).apply {
+                    putExtra("url", url)
+                }
+            )
+        }
 
         val newsService = retrofit.create(NewsService::class.java)
 
--- chapter5_news/app/src/main/java/com/example/chapter5_news/NewsAdapter.kt
+++ chapter5_news/app/src/main/java/com/example/chapter5_news/NewsAdapter.kt
@@ -8,12 +8,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.chapter5_news.databinding.ItemNewsBinding
 
-class NewsAdapter: ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
+class NewsAdapter(private val onClick: (String) -> Unit): ListAdapter<NewsModel, NewsAdapter.ViewHolder>(diffUtil) {
 
     inner class ViewHolder(private val binding: ItemNewsBinding): RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: NewsModel) {
             binding.titleTextView.text = item.title
+            binding.root.setOnClickListener {
+                onClick(item.link)
+            }
 
             Glide.with(binding.thumbnailImageView)
                 .load(item.imageUrl)
