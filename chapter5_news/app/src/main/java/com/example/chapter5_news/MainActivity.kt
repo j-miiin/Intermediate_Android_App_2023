dfa35acbcc3eef431934a0e6fc3e088a15ec9f43
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-03 21:46:26 +0900

    Part02 - Chapter05 뉴스 앱 완성

--- chapter5_news/app/src/main/java/com/example/chapter5_news/MainActivity.kt
+++ chapter5_news/app/src/main/java/com/example/chapter5_news/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chapter5_news.databinding.ActivityMainBinding
 import com.tickaroo.tikxml.TikXml
@@ -121,6 +122,8 @@ class MainActivity : AppCompatActivity() {
 
                 val list = response.body()?.channel?.items.orEmpty().transform()
                 newsAdapter.submitList(list)
+
+                binding.notFoundView.isVisible = list.isEmpty()
 
                 list.forEachIndexed { index, news ->
                     Thread {
