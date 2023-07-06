2f5b9200fbd84d69252b71f61da5b49e71096d05
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-06 18:25:01 +0900

    Part02 - Chapter06 로그인, 회원가입 구현

---
+++ chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/LoginActivity.kt
@@ -0,0 +1,64 @@
+package com.example.chapter6_chatting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.chapter6_chatting.databinding.ActivityLoginBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUpButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+
+            Firebase.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 회원가입 성공
+                        Toast.makeText(this, "회원가입에 성공했습니다", Toast.LENGTH_SHORT).show()
+                    } else {
+                        // 회원가입 실패
+                        Toast.makeText(this, "회원가입에 실패했습니다", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+
+        binding.signInButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "이메일과 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+            }
+
+            Firebase.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // 로그인 성공
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // 로그인 실패
+                        Log.e("LoginActivity", task.exception.toString())
+                        Toast.makeText(this, "로그인에 실패했습니다", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+}
