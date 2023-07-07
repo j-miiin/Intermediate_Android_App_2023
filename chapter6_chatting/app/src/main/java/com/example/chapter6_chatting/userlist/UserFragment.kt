08b9158703c5e6e3a326885e42b339b295e26950
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-07 21:31:09 +0900

    Part02 - Chapter06 유저 리스트 조회 구현

--- chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/userlist/UserFragment.kt
+++ chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/userlist/UserFragment.kt
@@ -6,8 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chapter6_chatting.Key.Companion.DB_USERS
 import com.example.chapter6_chatting.R
 import com.example.chapter6_chatting.databinding.FragmentUserBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class UserFragment : Fragment(R.layout.fragment_user) {
 
@@ -23,13 +30,30 @@ class UserFragment : Fragment(R.layout.fragment_user) {
             adapter = userListAdapter
         }
 
-        
+        val currentUserId = Firebase.auth.currentUser?.uid ?: ""
 
-        userListAdapter.submitList(
-            mutableListOf<UserItem?>().apply {
-                add(UserItem("11", "22", "33"))
-            }
-        )
+        Firebase.database.reference.child(DB_USERS)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+
+                    val userItemList = mutableListOf<UserItem>()
+
+                    snapshot.children.forEach {
+                        val user = it.getValue(UserItem::class.java)
+                        user ?: return
+
+                        if (user.userId != currentUserId) {
+                            userItemList.add(user)
+                        }
+                    }
+
+                    userListAdapter.submitList(userItemList)
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                }
+
+            })
     }
 
 }
