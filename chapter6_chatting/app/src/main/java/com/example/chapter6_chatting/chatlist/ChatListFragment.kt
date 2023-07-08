320bc8d24cc88063af5a6f363dd2733c127651bf
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-08 18:02:56 +0900

    Part02 - Chapter06 채팅룸 리스트 조회 구현

--- chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/chatlist/ChatListFragment.kt
+++ chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/chatlist/ChatListFragment.kt
@@ -4,8 +4,15 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.chapter6_chatting.Key.Companion.DB_CHAT_ROOM
 import com.example.chapter6_chatting.R
 import com.example.chapter6_chatting.databinding.FragmentChatlistBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
 
@@ -21,11 +28,22 @@ class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
             adapter = chatListAdapter
         }
 
-        chatListAdapter.submitList(
-            mutableListOf<ChatRoomItem?>().apply {
-                add(ChatRoomItem("11", "22", "33"))
+        val currentUserId = Firebase.auth.currentUser?.uid ?: return
+        val chatRoomsDB = Firebase.database.reference.child(DB_CHAT_ROOM).child(currentUserId)
+
+        chatRoomsDB.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val chatRoomList = snapshot.children.map {
+                    it.getValue(ChatRoomItem::class.java)
+                }
+                chatListAdapter.submitList(chatRoomList)
             }
-        )
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+        })
     }
 
 }
--- chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/mypage/MyPageFragment.kt
+++ chapter6_chatting/app/src/main/java/com/example/chapter6_chatting/mypage/MyPageFragment.kt
@@ -7,10 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.example.chapter6_chatting.Key.Companion.DB_USERS
 import com.example.chapter6_chatting.LoginActivity
 import com.example.chapter6_chatting.R
 import com.example.chapter6_chatting.databinding.FragmentMyPageBinding
+import com.example.chapter6_chatting.userlist.UserItem
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MyPageFragment : Fragment(R.layout.fragment_my_page) {
@@ -21,15 +24,29 @@ class MyPageFragment : Fragment(R.layout.fragment_my_page) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMyPageBinding.bind(view)
 
+        val currentUserId = Firebase.auth.currentUser?.uid ?: ""
+        val currentUserDB = Firebase.database.reference.child(DB_USERS).child(currentUserId)
+
+        currentUserDB.get().addOnSuccessListener {
+            val currentUserItem = it.getValue(UserItem::class.java) ?: return@addOnSuccessListener
+
+            binding.userNameEditText.setText(currentUserItem.username)
+            binding.descriptionEditText.setText(currentUserItem.description)
+        }
+
         binding.applyButton.setOnClickListener {
             val username = binding.userNameEditText.text.toString()
             val description = binding.descriptionEditText.text.toString()
 
             if (username.isEmpty()) {
                 Toast.makeText(context, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
-            } else {
-
+                return@setOnClickListener
             }
+
+            val user = mutableMapOf<String, Any>()
+            user["username"] = username
+            user["description"] = description
+            currentUserDB.updateChildren(user)
         }
 
         binding.signOutButton.setOnClickListener {
