96059eb7d03e3ab4ff9e1727929d82b569096e5f
j-miiin <62470991+j-miiin@users.noreply.github.com>
2023-07-13 23:57:44 +0900

    Part02 - Chapter07 날씨 정보 받아오기 구현

--- chapter7_weather/app/src/main/java/com/example/chapter7_weather/MainActivity.kt
+++ chapter7_weather/app/src/main/java/com/example/chapter7_weather/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.chapter7_weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.chapter7_weather.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -19,5 +23,27 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("http://apis.data.go.kr/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+
+        val service = retrofit.create(WeatherService::class.java)
+
+        service.getVillageForecast(
+            serviceKey = "",
+            baseDate = "20230713",
+            baseTime = "0200",
+            nx = 55,
+            ny = 127
+        ).enqueue(object : Callback<WeatherEntity> {
+            override fun onResponse(call: Call<WeatherEntity>, response: Response<WeatherEntity>) {
+                val forecastList = response.body()?.response?.body?.items?.forecastEntities.orEmpty()
+
+                for (forecast in forecastList) {
+                    Log.e("Forecast", forecast.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherEntity>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
